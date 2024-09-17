52171ee9f4e3a4b4c49ad72c1ee7c3f7919ce17c
Valy1x <lulipsa2.0@gmail.com>
2024-09-17 21:28:55 +0300

    Update 2_part3.cpp

--- 2_part3.cpp
+++ 2_part3.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 int main()
 {
-    ofstream file;          // поток для записи
-    file.open("numbers.txt");      // открываем файл для записи
-    while (!file.eof()) {
-            file >> x;
-            vector<unsigned char> list(8, 0);
-        }
+	ofstream file;          // поток для записи
+	file.open("numbers.txt");      // открываем файл для записи
+	vector<unsigned char> list(8, 0);
+	while (!file.eof()) {
+		file >> x;
+	}
 }
